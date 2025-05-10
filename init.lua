f6e6d2adda988b551af2c6521615eddf71335b36
Nathan <nathanbarber6@gmail.com>
2025-05-10 13:59:18 -0400

    Update key binds for window nav

--- init.lua
+++ init.lua
@@ -13,12 +13,13 @@ vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live gr
 vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
 
-vim.keymap.set('n', '<leader>nt', ':Neotree<CR>', { desc = 'Open Neotree' })
-vim.keymap.set('n', '<leader>tt', ':ToggleTerm<CR>', { desc = 'Open Terminal' })
+vim.keymap.set('n', '<leader>nf', ':Neotree<CR>', { desc = 'Open Neotree' })
+vim.keymap.set('n', '<leader>nt', ':ToggleTerm<CR>', { desc = 'Open Terminal' })
 
-vim.keymap.set('n', '<leader>h', '<C-w>h', {})
-vim.keymap.set('n', '<leader>j', '<C-w>j', {})
-vim.keymap.set('n', '<leader>k', '<C-w>k', {})
-vim.keymap.set('n', '<leader>l', '<C-w>l', {})
+vim.keymap.set('n', '<leader>kk', '<C-w>k', { desc = "Move to upper window" })
+vim.keymap.set('n', '<leader>jj', '<C-w>j', { desc = "Move to lower window" })
+vim.keymap.set('n', '<leader>hh', '<C-w>h', { desc = "Move to left window" })
+vim.keymap.set('n', '<leader>ll', '<C-w>l', { desc = "Move to right window" })
+
 
 vim.keymap.set('t', '<Esc>', '<C-\\><C-n>', { noremap = true, silent = true })
--- lua/plugins/spec.lua
+++ lua/plugins/spec.lua
@@ -27,4 +27,23 @@ return {
 			-- fill any relevant options here
 		},
 	},
+	{
+		"folke/which-key.nvim",
+		event = "VeryLazy",
+		opts = {
+			-- your configuration comes here
+			-- or leave it empty to use the default settings
+			-- refer to the configuration section below
+		},
+		keys = {
+			{
+				"<leader>?",
+				function()
+					require("which-key").show({ global = false })
+				end,
+				desc = "Buffer Local Keymaps (which-key)",
+			},
+		},
+	},
+	{ "nvim-tree/nvim-web-devicons", opts = {} },
 }
