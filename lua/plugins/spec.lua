a52e2fdc0f55e01ca232a138d33a030fec49990b
Nathan <nathanbarber6@gmail.com>
2025-05-11 12:17:00 -0400

    Autocomplete enabled!

--- lua/plugins/spec.lua
+++ lua/plugins/spec.lua
@@ -65,28 +65,31 @@ return {
 			{'hrsh7th/cmp-buffer'},
 			{'hrsh7th/cmp-nvim-lsp'},
     },
--- 		setup = {
--- 			snippet = {
--- 				expand = function(args)
--- 					vim.snippet.expand(args.body)
--- 				end
--- 			},
--- 			window = {
--- 				completion = cmp.config.window.bordered(),
--- 				documentation = cmp.config.window.bordered()
--- 			},
--- 			mapping = cmp.mapping.preset.insert({
--- 				['<C-b>'] = cmp.mapping.scroll_docs(-4),
--- 				['<C-f>'] = cmp.mapping.scroll_docs(4),
--- 				['<C-Space>'] = cmp.mapping.complete(),
--- 				['<C-e>'] = cmp.mapping.abort(),
--- 				['<CR>'] = cmp.mapping.confirm({ select = true }),
--- 			}),
--- 			sources = cmp.config.sources({
--- 				{ name = "nvim_lsp" },
--- 			}, {
--- 				{ name = "buffer" }
--- 			})
--- 		},
+		opts = function()
+			local cmp = require("cmp")
+			return {
+				snippet = {
+					expand = function(args)
+						vim.snippet.expand(args.body)
+					end
+				},
+				window = {
+					completion = cmp.config.window.bordered(),
+					documentation = cmp.config.window.bordered()
+				},
+				mapping = cmp.mapping.preset.insert({
+					['<C-b>'] = cmp.mapping.scroll_docs(-4),
+					['<C-f>'] = cmp.mapping.scroll_docs(4),
+					['<C-Space>'] = cmp.mapping.complete(),
+					['<C-e>'] = cmp.mapping.abort(),
+					['<CR>'] = cmp.mapping.confirm({ select = true }),
+				}),
+				sources = cmp.config.sources({
+					{ name = "nvim_lsp" },
+				}, {
+					{ name = "buffer" }
+				})
+			}
+		end
 	},
 }
